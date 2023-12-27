4cd8c4c9caf3b33053894522907e1bf48c706cb7
Shravanipagidi <shravanireddy7789@gmail.com>
2023-12-27 13:25:14 +0530

    All files added

---
+++ src/test/java/selenium_jenkins_exercise2/BaseTest2.java
@@ -0,0 +1,34 @@
+package selenium_jenkins_exercise2;
+
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest2 {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+
+        System.out.println("launching app on chrome browser -----using TestNG");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void teardown() throws Exception {
+
+        Thread.sleep(5000);
+        driver.close();
+        System.out.println("closing an app");
+
+    }
+
+}
---
+++ src/test/java/selenium_jenkins_exercise2/LoginLogout.java
@@ -0,0 +1,50 @@
+package selenium_jenkins_exercise2;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class LoginLogout extends BaseTest2{
+    
+    
+    @Test
+    public void verifyLogin() throws Exception {
+        
+        Thread.sleep(3000);
+        
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+        
+        
+        Thread.sleep(2000);
+        System.out.println("application URL after login: " + driver.getCurrentUrl());
+        
+        
+        driver.findElement(By.className("oxd-userdropdown-name")).click();
+        Thread.sleep(2000);
+        
+        
+        
+        
+        
+//      driver.findElement(By.linkText("Logout")).click();
+        
+        
+//      driver.findElement(By.partialLinkText("Logo")).click();
+        
+        
+        driver.findElement(By.partialLinkText("ogo")).click();
+        
+        
+        Thread.sleep(3000);
+        System.out.println("application URL after logout: " + driver.getCurrentUrl());
+        
+        
+        
+    }
+
+}
